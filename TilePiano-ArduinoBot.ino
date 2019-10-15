c40725e25a687e61fe1853e4d592b2467d89e8da
Krzysztof PIaskowy <krzychu2956@gmail.com>
2019-10-15 21:57:05 +0200

    android application to calibration foto sensors

--- TilePiano-ArduinoBot.ino
+++ TilePiano-ArduinoBot.ino
@@ -1,17 +1,50 @@
-#define detectorPin 5
-#define touchPin 6
+#define DEBUG 1
 
-int detectorValue = 0;
+#define false 0
+#define true 1
+
+#define detector1Pin A0
+#define detector2Pin A1
+#define touchPin 6
+#define buttonPin 2
+
+volatile byte programState = false;
+int detector1Value = 0;
+int detector2Value = 0;
+unsigned long timeStamp = 0;
+int interruptInterval = 500;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(detectorPin, INPUT);
   pinMode(touchPin, OUTPUT);
+
+  timeStamp = millis() - interruptInterval;
+  pinMode(buttonPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(buttonPin), bottonAction, FALLING);
 }
 
 void loop() {
-  Serial.println("mleko");
+  if(!programState) return;
+
+  Serial.print("Detector 1: ");
+  detector1Value = analogRead(A0);
+  Serial.println(detector1Value);
+
+  Serial.print("Detector 2: ");
+  detector2Value = analogRead(A1);
+  Serial.println(detector2Value);
+  
+  Serial.println("");
   delay(1000);
-  detectorValue = analogRead(detectorPin);
-  Serial.println(detectorValue);
+}
+
+void bottonAction() {
+  if(millis() - timeStamp <= interruptInterval) return;
+  timeStamp = millis();
+  programState = !programState;
+  
+  #if DEBUG
+    Serial.print("Button press ");
+    Serial.println(programState);
+  #endif
 }
