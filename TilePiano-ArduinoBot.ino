5529236da04843b654a2986571159254311ea04f
Krzysztof Piaskowy <krzychu2956@gmail.com>
2019-12-12 13:12:49 +0100

    Update TilePiano-ArduinoBot.ino

--- TilePiano-ArduinoBot.ino
+++ TilePiano-ArduinoBot.ino
@@ -1,3 +1,5 @@
+#include <Servo.h>
+
 #define DEBUG 1
 
 #define GAME_MODE 0
@@ -24,137 +26,137 @@ int touchsPin[4] = {4, 5, 6, 7};
 int sensorsBar[2] = {8, 9};
 
 void calibrate() {
-	calibrateState = 1;
-	int sumSensorValue = 0;
-	int sensorMeasurmentCount = 5;
+  calibrateState = 1;
+  int sumSensorValue = 0;
+  int sensorMeasurmentCount = 5;
 
-	selectSensorBar(0);
-	for(int detectorNumber = 0; detectorNumber < 8; detectorNumber++) {
-		if(detectorNumber == 4) {
-			selectSensorBar(1);
-		}
+  selectSensorBar(0);
+  for(int detectorNumber = 0; detectorNumber < 8; detectorNumber++) {
+    if(detectorNumber == 4) {
+      selectSensorBar(1);
+    }
 
-		sumSensorValue = 0;
-		for(int i = 0; i < sensorMeasurmentCount; i++) {
-			sumSensorValue += analogRead(detector);
-			delay(50);
-		}
-		blackBlockAcceptable[detectorNumber + blockShift] = sumSensorValue / sensorMeasurmentCount;
-		click_classic(0);
-	}
+    sumSensorValue = 0;
+    for(int i = 0; i < sensorMeasurmentCount; i++) {
+      sumSensorValue += analogRead(detectorsPin[i]);
+      delay(50);
+    }
+    blackBlockAcceptable[detectorNumber + blockShift] = sumSensorValue / sensorMeasurmentCount;
+    click_classic(0);
+  }
 
-	calibrateState = 0;
+  calibrateState = 0;
 }
 
 void calibrateAction() {
-	if(millis() - timestampCalibrateButton <= interruptInterval || calibrateState == 1) return;
-	timestampCalibrateButton = millis();
+  if(millis() - timestampCalibrateButton <= interruptInterval || calibrateState == 1) return;
+  timestampCalibrateButton = millis();
 
-	#if DEBUG
-		Serial.print("Button press ");
-		Serial.println(programState);
-	#endif
+  #if DEBUG
+    Serial.print("Button press ");
+    Serial.println(programState);
+  #endif
 
-	#if DEBUG
-		Serial.println("Calibrate START!");
-	#endif
+  #if DEBUG
+    Serial.println("Calibrate START!");
+  #endif
 
-	calibrate();
+  calibrate();
 
-	#if DEBUG
-		Serial.println("Calibrate END!");
-	#endif
+  #if DEBUG
+    Serial.println("Calibrate END!");
+  #endif
 }
 
 void stateAction() {
-	if(millis() - timestampStateButton <= interruptInterval) return;
-	timestampStateButton = millis();
-	programState = !programState;
+  if(millis() - timestampStateButton <= interruptInterval) return;
+  timestampStateButton = millis();
+  programState = !programState;
 
-	#if DEBUG
-		Serial.print("Button press ");
-		Serial.println(programState);
-	#endif
+  #if DEBUG
+    Serial.print("Button press ");
+    Serial.println(programState);
+  #endif
 }
 
 void setup() {
-	Serial.begin(9600);
+  Serial.begin(9600);
 
-	for(int pin : sensorsBar) {
-		pinMode(pin, OUTPUT);
-	}
+  for(int pin : sensorsBar) {
+    pinMode(pin, OUTPUT);
+  }
 
-	int i = 0;
-	for(Servo servo : touchsServo) {
-		servo.attach(touchsPin[i]);
-		i++;
-	}
+  int i = 0;
+  for(Servo servo : touchsServo) {
+    servo.attach(touchsPin[i]);
+    i++;
+  }
 
-	timestampCalibrateButton = millis() - interruptInterval;
-	timestampStateButton = millis() - interruptInterval;
+  timestampCalibrateButton = millis() - interruptInterval;
+  timestampStateButton = millis() - interruptInterval;
 
-	pinMode(calibrateButton, INPUT_PULLUP);
-	attachInterrupt(digitalPinToInterrupt(calibrateButton), calibrateAction, FALLING);
+  pinMode(calibrateButton, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(calibrateButton), calibrateAction, FALLING);
 
-	pinMode(startButton, INPUT_PULLUP);
-	attachInterrupt(digitalPinToInterrupt(startButton), stateAction, FALLING);
+  pinMode(startButton, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(startButton), stateAction, FALLING);
 
-	selectSensorBar(0);
+  selectSensorBar(0);
 
-	#if DEBUG
-		Serial.print("Initilization: OK");
-	#endif
+  #if DEBUG
+    Serial.print("Initilization: OK");
+  #endif
 }
 
 void selectSensorBar(int barId) {
-	if(barId == 0) {
-		digitalWrite(sensorsBar[0], HIGH);
-		digitalWrite(sensorsBar[1], LOW);
-		blockShift = 0;
-	}
-	else {
-		digitalWrite(sensorsBar[1], HIGH);
-		digitalWrite(sensorsBar[0], LOW);
-		blockShift = 4;
-	}
+  if(barId == 0) {
+    digitalWrite(sensorsBar[0], HIGH);
+    digitalWrite(sensorsBar[1], LOW);
+    blockShift = 0;
+  }
+  else {
+    digitalWrite(sensorsBar[1], HIGH);
+    digitalWrite(sensorsBar[0], LOW);
+    blockShift = 4;
+  }
 
-	#if DEBUG
-		Serial.print("Select bar: ");
-		Serial.println(barId);
-	#endif
+  #if DEBUG
+    Serial.print("Select bar: ");
+    Serial.println(barId);
+  #endif
 }
 
 // =======================================================================
 /* classic */
 // =======================================================================
 void click_classic(int touchEmulator) {
-	#if DEBUG
-		Serial.print("Click ");
-		Serial.println(touchPin);
-	#endif
+  #if DEBUG
+    Serial.print("Click ");
+    Serial.println(touchEmulator);
+  #endif
 
-	touchsServo[touchEmulator].write(servoAngle);
-	delay(servoDelay);
-	touchsServo[touchEmulator].write(servoAngle);
+  touchsServo[touchEmulator].write(servoAngle);
+  delay(servoDelay);
+  touchsServo[touchEmulator].write(servoAngle);
 }
 
 int detect_classic() {
-	for(int i = 0; i < 4; i++) {
-		if(analogRead(detectorsPin[i]) - blackBlockAcceptable[i + blockShift] < detectorTrashold) {
-			#if DEBUG
-				Serial.print("Detected ");
-				Serial.println(detectedTile);
-			#endif
+  for(int i = 0; i < 4; i++) {
+    if(analogRead(detectorsPin[i]) - blackBlockAcceptable[i + blockShift] < detectorTrashold) {
+      #if DEBUG
+        Serial.print("Detected ");
+        Serial.println(i);
+      #endif
 
-			return i;
-		}
-	}
+      return i;
+    }
+  }
 
-	#if DEBUG
-		Serial.println("Error, not detected!");
-	#endif
+  #if DEBUG
+    Serial.println("Error, not detected!");
+  #endif
 
-	return 0;
+  return 0;
 }
 
 // =======================================================================
@@ -176,164 +178,164 @@ int sensorDistance = 200;
 float magicNumber = 1.1;
 
 void click_arcade(int touchEmulator) {
-	#if DEBUG
-		Serial.print("Click ");
-		Serial.println(touchEmulator);
-	#endif
+  #if DEBUG
+    Serial.print("Click ");
+    Serial.println(touchEmulator);
+  #endif
 
-	touchsServo[touchEmulator].write(servoAngle);
-	delay(servoDelay);
-	touchsServo[touchEmulator].write(servoAngle);
-	detectBlock = 0;
+  touchsServo[touchEmulator].write(servoAngle);
+  delay(servoDelay);
+  touchsServo[touchEmulator].write(servoAngle);
+  detectBlock = 0;
 }
 
 int detect_arcade() {
-	if(detectBlock) return -1;
+  if(detectBlock) return -1;
 
-	for(int i = 0; i < 4; i++) {
-		if(analogRead(detectorsPin[i]) - blackBlockAcceptable[i + blockShift] < detectorTrashold) {
-			#if DEBUG
-				Serial.print("Detected ");
-				Serial.println(detectedTile);
-			#endif
+  for(int i = 0; i < 4; i++) {
+    if(analogRead(detectorsPin[i]) - blackBlockAcceptable[i + blockShift] < detectorTrashold) {
+      #if DEBUG
+        Serial.print("Detected ");
+        Serial.println(i);
+      #endif
 
-			detectBlock = 1;
-			detectedTime = millis();
+      detectBlock = 1;
+      detectedTime = millis();
 
-			counter++;
-			if(!speedCalculateStart && counter % tickCountTo_calculateSpeed == 0) {
-				speedCalculateStart = true;
-			}
+      counter++;
+      if(!speedCalculateStart && counter % tickCountTo_calculateSpeed == 0) {
+        speedCalculateStart = true;
+      }
 
-			calculateSpeed();
+      calculateSpeed();
 
-			return i;
-		}
-	}
-	#if DEBUG
-	Serial.println("Error, not detected!");
-	#endif
+      return i;
+    }
+  }
+  #if DEBUG
+  Serial.println("Error, not detected!");
+  #endif
 }
 
 int detectSimple() {
-	for(int i = 0; i < 4; i++) {
-	if(analogRead(detectorsPin[i]) - blackBlockAcceptable[i + blockShift] < detectorTrashold) {
-		#if DEBUG
-			Serial.print("Detected ");
-			Serial.println(i);
-		#endif
+  for(int i = 0; i < 4; i++) {
+  if(analogRead(detectorsPin[i]) - blackBlockAcceptable[i + blockShift] < detectorTrashold) {
+    #if DEBUG
+      Serial.print("Detected ");
+      Serial.println(i);
+    #endif
 
-		return i;
-	}
-	}
-	#if DEBUG
-		Serial.println("Error, not detected!");
-	#endif
+    return i;
+  }
+  }
+  #if DEBUG
+    Serial.println("Error, not detected!");
+  #endif
 }
 
 int isBlack(int tileNumber) {
-	if(analogRead(detectorsPin[tileNumber]) - blackBlockAcceptable[tileNumber + blockShift] < detectorTrashold) {
-		#if DEBUG
-			Serial.print("Detected BLACK for tile");
-			Serial.println(tileNumber);
-		#endif
-		return true;
-	}
-	#if DEBUG
-		Serial.print("Detected WHITE for tile");
-		Serial.println(tileNumber);
-	#endif
+  if(analogRead(detectorsPin[tileNumber]) - blackBlockAcceptable[tileNumber + blockShift] < detectorTrashold) {
+    #if DEBUG
+      Serial.print("Detected BLACK for tile");
+      Serial.println(tileNumber);
+    #endif
+    return true;
+  }
+  #if DEBUG
+    Serial.print("Detected WHITE for tile");
+    Serial.println(tileNumber);
+  #endif
 
-	return false;
+  return false;
 }
 
 void calculateSpeed() {
-	//ignore calculate
-	if(!speedCalculateStart) {
-		observedTile = -1;
-		return;
-	}
+  //ignore calculate
+  if(!speedCalculateStart) {
+    observedTile = -1;
+    return;
+  }
 
-	//detect white tile
-	if(observedTile == -1) {
-		selectSensorBar(1);
-		observedTile = detectSimple() + 1 % 4;
-		selectSensorBar(0);
-		return;
-	}
+  //detect white tile
+  if(observedTile == -1) {
+    selectSensorBar(1);
+    observedTile = detectSimple() + 1 % 4;
+    selectSensorBar(0);
+    return;
+  }
 
-	if(speedTimestamp != 0) {
-		//wait for white tile change in black
-		selectSensorBar(1);
-		if(isBlack(observedTile)) {
-			//start mensurement
-			speedTimestamp = millis();
-		}
-		selectSensorBar(0);
-		return;
-	}
+  if(speedTimestamp != 0) {
+    //wait for white tile change in black
+    selectSensorBar(1);
+    if(isBlack(observedTile)) {
+      //start mensurement
+      speedTimestamp = millis();
+    }
+    selectSensorBar(0);
+    return;
+  }
 
-	if(isBlack(observedTile)) {
-		timeToWait = (millis() - speedTimestamp) * magicNumber;
-		speedCalculateStart = 0;
-	}
+  if(isBlack(observedTile)) {
+    timeToWait = (millis() - speedTimestamp) * magicNumber;
+    speedCalculateStart = 0;
+  }
 }
 
 void loop() {
-	if(!programState) return;
+  if(!programState) return;
 
-	//clasic mode
-	if(GAME_MODE == 0) {
-		int currentTile = 0;
-		int nextTile = 3;
+  //clasic mode
+  if(GAME_MODE == 0) {
+    int currentTile = 0;
+    int nextTile = 3;
 
-		while(programState) {
-			currentTile = nextTile;
-			nextTile = detect_classic();
-			#if DEBUG
-				testDetector();
-				Serial.print("Current tile: ");
-				Serial.print(currentTile);
-				Serial.print(", next tile: ");
-				Serial.println(nextTile);
-			#endif
-			click_classic(currentTile);
-		}
-	}
+    while(programState) {
+      currentTile = nextTile;
+      nextTile = detect_classic();
+      #if DEBUG
+        testDetector();
+        Serial.print("Current tile: ");
+        Serial.print(currentTile);
+        Serial.print(", next tile: ");
+        Serial.println(nextTile);
+      #endif
+      click_classic(currentTile);
+    }
+  }
 
-	//arcade mode
-	if(GAME_MODE == 1) {
-		int detectedResult = 0;
-		int currentTile;
-		int nextTile;
+  //arcade mode
+  if(GAME_MODE == 1) {
+    int detectedResult = 0;
+    int currentTile;
+    int nextTile;
 
-		Serial.print("analize_first");
-		while(true) {
-			if(Serial.available() > 0) {
-				currentTile = Serial.read();
-				break;
-			}
-			delay(100);
-		}
+    Serial.print("analize_first");
+    while(true) {
+      if(Serial.available() > 0) {
+        currentTile = Serial.read();
+        break;
+      }
+      delay(100);
+    }
 
-		while(programState) {
-			detectedResult = detect_arcade();
-			if(detectedResult >= 0) {
-				nextTile = detectedResult;
-			}
+    while(programState) {
+      detectedResult = detect_arcade();
+      if(detectedResult >= 0) {
+        nextTile = detectedResult;
+      }
 
-			if(millis() - detectedTime > timeToWait) {
-				click_arcade(currentTile);
-				currentTile = nextTile;
-			}
-		}
-	}
+      if(millis() - detectedTime > timeToWait) {
+        click_arcade(currentTile);
+        currentTile = nextTile;
+      }
+    }
+  }
 }
 
 void testDetector() {
-	for(int i = 0; i < 4; i++) {
-		Serial.print(analogRead(detectorsPin[i]));
-		Serial.print(" | ");
-	}
-	Serial.println();
+  for(int i = 0; i < 4; i++) {
+    Serial.print(analogRead(detectorsPin[i]));
+    Serial.print(" | ");
+  }
+  Serial.println();
 }
