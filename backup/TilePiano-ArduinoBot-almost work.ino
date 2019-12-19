1c7a59c742e381b6d22217ad0902fc09d4924461
Krzysztof PIaskowy <krzychu2956@gmail.com>
2019-12-19 22:43:10 +0100

    wow - it's work!

--- backup/TilePiano-ArduinoBot-almost work.ino
+++ backup/TilePiano-ArduinoBot-almost work.ino
@@ -8,7 +8,7 @@
 
 #define calibrateButton 2
 #define startButton 3
-#define DETECTOR_TRASHOLD 3
+#define DETECTOR_TRASHOLD 5
 #define INTERRUPT_INVERTAL 500
 #define SERVO_DELAY 100
 
@@ -30,7 +30,7 @@ int sensorsBar[2] = {8, 9};
 Servo touchsServo[4];
 int servoPin[4] = {4, 5, 6, 7};
 int servoTopPosition[4] = {35, 30, 50, 25};
-int servoBottomPosition[4] = {8, 59, 24, 55};
+int servoBottomPosition[4] = {8, 57, 24, 55};
 
 unsigned long detectedTime = 0;
 unsigned long timeToWait = 300;
@@ -62,7 +62,7 @@ void setup() {
 
   #if FORCE_CALIBRATION
     for(int i = 0; i < 8; i++) {
-      blackBlockAcceptable[i] = 10;
+      blackBlockAcceptable[i] = 20;
     }
   #endif
 
@@ -111,9 +111,37 @@ void loop() {
       #endif
 
       #if DEBUG and MANUAL_INPUT == 0
-        delay(500);
+        delay(1000);
         Serial.println("");
       #endif
+    }
+  }
+
+  //arcade mode
+  if(GAME_MODE == 1) {
+    int detectedResult = 0;
+    int currentTile;
+    int nextTile;
+
+    Serial.print("analize_first");
+    while(true) {
+      if(Serial.available() > 0) {
+        currentTile = Serial.read();
+        break;
+      }
+      delay(100);
+    }
+
+    while(programState) {
+      detectedResult = detect_arcade();
+      if(detectedResult >= 0) {
+        nextTile = detectedResult;
+      }
+
+      if(millis() - detectedTime > timeToWait) {
+        click_arcade(currentTile);
+        currentTile = nextTile;
+      }
     }
   }
 }
@@ -249,4 +277,125 @@ int detect_classic() {
   }
 
   return barDetect[0];
+}
+
+// =======================================================================
+/* arcade */
+// =======================================================================
+#define tickCountTo_calculateSpeed 10
+#define distanceBetweenDetectorBars 200
+
+unsigned int counter = 0;
+int detectBlock = 0;
+
+int speedCalculateStart = false;
+int observedTile = -1;
+int blackTile = -1;
+unsigned long speedTimestamp = 0;
+int sensorDistance = 200;
+float magicNumber = 1.1;
+
+void click_arcade(int servoIndex) {
+  #if DEBUG
+    Serial.print("Click ");
+    Serial.println(servoIndex);
+  #endif
+
+  touchsServo[servoIndex].write(servoBottomPosition[servoIndex]);
+  delay(SERVO_DELAY);
+  touchsServo[servoIndex].write(servoTopPosition[servoIndex]);
+
+  detectBlock = 0;
+}
+
+int detect_arcade() {
+  if(detectBlock) return -1;
+
+  for(int i = 0; i < 4; i++) {
+    if(analogRead(detectorsPin[i]) - blackBlockAcceptable[i + blockShift] < DETECTOR_TRASHOLD) {
+      #if DEBUG
+        Serial.print("Detected ");
+        Serial.println(i);
+      #endif
+
+      detectBlock = 1;
+      detectedTime = millis();
+
+      counter++;
+      if(!speedCalculateStart && counter % tickCountTo_calculateSpeed == 0) {
+        speedCalculateStart = true;
+      }
+
+      calculateSpeed();
+
+      return i;
+    }
+  }
+  #if DEBUG
+  Serial.println("Error, not detected!");
+  #endif
+}
+
+int detectSimple() {
+  for(int i = 0; i < 4; i++) {
+  if(analogRead(detectorsPin[i]) - blackBlockAcceptable[i + blockShift] < DETECTOR_TRASHOLD) {
+    #if DEBUG
+      Serial.print("Detected ");
+      Serial.println(i);
+    #endif
+
+    return i;
+  }
+  }
+  #if DEBUG
+    Serial.println("Error, not detected!");
+  #endif
+}
+
+int isBlack(int tileNumber) {
+  if(analogRead(detectorsPin[tileNumber]) - blackBlockAcceptable[tileNumber + blockShift] < DETECTOR_TRASHOLD) {
+    #if DEBUG
+      Serial.print("Detected BLACK for tile");
+      Serial.println(tileNumber);
+    #endif
+    return true;
+  }
+  #if DEBUG
+    Serial.print("Detected WHITE for tile");
+    Serial.println(tileNumber);
+  #endif
+
+  return false;
+}
+
+void calculateSpeed() {
+  //ignore calculate
+  if(!speedCalculateStart) {
+    observedTile = -1;
+    return;
+  }
+
+  //detect white tile
+  if(observedTile == -1) {
+    selectSensorBar(1);
+    observedTile = detectSimple() + 1 % 4;
+    selectSensorBar(0);
+    return;
+  }
+
+  if(speedTimestamp != 0) {
+    //wait for white tile change in black
+    selectSensorBar(1);
+    if(isBlack(observedTile)) {
+      //start mensurement
+      speedTimestamp = millis();
+    }
+    selectSensorBar(0);
+    return;
+  }
+
+  if(isBlack(observedTile)) {
+    timeToWait = (millis() - speedTimestamp) * magicNumber;
+    speedCalculateStart = 0;
+  }
 }
